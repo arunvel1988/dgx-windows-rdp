a63ad800b9eaaa3c13beaf7c6e0a59bece14d1d6
Arunvel Arunachalam <aryan.arunachalam@gmail.com>
2025-11-10 22:57:28 -0500

    Update install_rdp.sh

--- install_rdp.sh
+++ install_rdp.sh
@@ -3,6 +3,7 @@ sudo apt upgrade -y
 
 sudo apt install -y xrdp
 sudo apt install -y xfce4 xfce4-goodies
+sudo apt install -y chromium-browser
 echo "startxfce4" > ~/.xsession
 sudo systemctl enable xrdp
 sudo systemctl start xrdp
